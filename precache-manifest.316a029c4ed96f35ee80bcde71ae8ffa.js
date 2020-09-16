5b279ecf0b1367c60b4f387ed76446c80aa2e42f
Sergey <serejabgdn@gmail.com>
2020-09-16 15:36:47 +0300

    Updates

---
+++ precache-manifest.316a029c4ed96f35ee80bcde71ae8ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe306cc9af9322e81c0c780c46b9faa",
+    "url": "/serejabogdan/mahjong-react.git/index.html"
+  },
+  {
+    "revision": "a105542dec0e5c8c4251",
+    "url": "/serejabogdan/mahjong-react.git/static/css/main.0a001345.chunk.css"
+  },
+  {
+    "revision": "216b62e8010ebe93da65",
+    "url": "/serejabogdan/mahjong-react.git/static/js/2.e49953ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/serejabogdan/mahjong-react.git/static/js/2.e49953ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a105542dec0e5c8c4251",
+    "url": "/serejabogdan/mahjong-react.git/static/js/main.898b27b2.chunk.js"
+  },
+  {
+    "revision": "9f8837faf5b349c8cb5a",
+    "url": "/serejabogdan/mahjong-react.git/static/js/runtime-main.7c0874b4.js"
+  }
+]);
